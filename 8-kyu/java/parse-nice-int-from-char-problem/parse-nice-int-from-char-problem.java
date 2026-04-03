5fa8a697a6cd01be7fa3aa97d23856ff31772996
ildar <il.akhmetzhanov@yandex.kz>
2026-04-03 11:57:03 +0500

    Add solution - CodeHub

---
+++ 8-kyu/java/parse-nice-int-from-char-problem/parse-nice-int-from-char-problem.java
@@ -0,0 +1,9 @@
+public class CharProblem {
+  public static int howOld(final String herOld) {
+​
+  
+    String[] newLine = herOld.split(" ");
+      return Integer.parseInt(newLine[0]);
+  
+  }
+}

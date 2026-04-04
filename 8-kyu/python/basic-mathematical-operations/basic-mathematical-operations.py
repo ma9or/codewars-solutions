e7cdc6bf32032daf8725e5fbab1c451145f673bb
ildar <il.akhmetzhanov@yandex.kz>
2026-04-04 20:03:21 +0500

    Add solution - CodeHub

---
+++ 8-kyu/python/basic-mathematical-operations/basic-mathematical-operations.py
@@ -0,0 +1,6 @@
+def basic_op(operator, value1, value2):
+    match operator:
+        case "+": return value1 + value2
+        case "-": return value1 - value2
+        case "*": return value1 * value2
+        case "/": return value1 / value2

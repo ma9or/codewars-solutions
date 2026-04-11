5f4731c9358ee30c2a8af6595c382dcef9ed7810
ildar <il.akhmetzhanov@yandex.kz>
2026-04-11 13:07:30 +0500

    Add solution - CodeHub

---
+++ 8-kyu/python/determine-offspring-sex-based-on-genes-xx-and-xy-chromosomes/determine-offspring-sex-based-on-genes-xx-and-xy-chromosomes.py
@@ -0,0 +1,4 @@
+def chromosome_check(chromosome):
+    if chromosome == "XX":return "Congratulations! You're going to have a daughter."
+    else: return "Congratulations! You're going to have a son."
+​

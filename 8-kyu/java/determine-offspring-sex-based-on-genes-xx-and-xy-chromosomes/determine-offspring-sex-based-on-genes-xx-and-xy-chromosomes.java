47df35618ee008409845a796479a004cc0556f99
ildar <il.akhmetzhanov@yandex.kz>
2026-04-11 13:10:45 +0500

    Add solution - CodeHub

---
+++ 8-kyu/java/determine-offspring-sex-based-on-genes-xx-and-xy-chromosomes/determine-offspring-sex-based-on-genes-xx-and-xy-chromosomes.java
@@ -0,0 +1,7 @@
+public class Kata {
+  public static String chromosomeCheck(String sperm) {
+    if (sperm == "XX") {return "Congratulations! You're going to have a daughter.";}
+    else {return "Congratulations! You're going to have a son.";}
+    
+  }
+}

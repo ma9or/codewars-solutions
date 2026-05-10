58410092a7de7bba464d5f1388a72b3430b46c9b
ildar <il.akhmetzhanov@yandex.kz>
2026-05-10 18:43:31 +0500

    Add solution - CodeHub

---
+++ 8-kyu/javascript/coding-3min-jumping-dutch-act/coding-3min-jumping-dutch-act.js
@@ -0,0 +1,14 @@
+function sc(floor){
+    let scrim = "";
+    const wordFirst = "Aa~ ";
+    const wordSecond = "Pa!";
+    const wordTherd = " Aa!";
+​
+    if (floor <= 1) {return ""};
+    if (floor >= 2 && floor <= 6) {
+        scrim += wordFirst.repeat(floor - 1) + wordSecond + wordTherd;
+    } else {
+        scrim += wordFirst.repeat(floor - 1) + wordSecond;
+    }
+  return scrim;
+}

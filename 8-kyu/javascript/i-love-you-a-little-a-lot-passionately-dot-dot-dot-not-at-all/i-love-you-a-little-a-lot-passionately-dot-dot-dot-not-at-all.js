2519926e8e6408516c4837f2a3174fd92b720352
ildar <il.akhmetzhanov@yandex.kz>
2026-05-17 20:27:33 +0500

    Update solution - CodeHub

--- 8-kyu/javascript/i-love-you-a-little-a-lot-passionately-dot-dot-dot-not-at-all/i-love-you-a-little-a-lot-passionately-dot-dot-dot-not-at-all.js
+++ 8-kyu/javascript/i-love-you-a-little-a-lot-passionately-dot-dot-dot-not-at-all/i-love-you-a-little-a-lot-passionately-dot-dot-dot-not-at-all.js
@@ -1,19 +1,18 @@
 function howMuchILoveYou(nbPetals) {
   let newPhrase = '';
-  const arr = ["", "I love you", "a little", "a lot", "passionately", "madly", "not at all"]
-  
- nbPetals = nbPetals % 6;
-  if (nbPetals === 0) nbPetals = 6;
-  
-  if (nbPetals < arr.length) {
-    for (let i = 1; i < nbPetals + 1; i++){
-      newPhrase = arr[i];
+    const arr = ["", "I love you", "a little", "a lot", "passionately", "madly", "not at all"]
+    while( nbPetals > 6) {
+      nbPetals -= 6;
     }
-  } else {
-    for (let i = 1; i < (nbPetals + 1) - 6; i++){
-      newPhrase = arr[i];
+    
+    if ( nbPetals < arr.length) {
+      for (let i = 1; i < nbPetals + 1; i++){
+        newPhrase = arr[i];
+      }
+    } else {
+      for (let i = 1; i < (nbPetals + 1) - 6; i++){
+        newPhrase = arr[i];
+      }
     }
-  }
   return newPhrase;
 }
-​
